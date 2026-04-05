4fa0e8e10e4baff79c993e1d07d4186f4469945f
Joey <joas001@ess.ais.ac.nz>
2026-04-05 16:26:59 +1200

    Frontend deployment in vercel fix; correct the backend url

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
+const BASE_URL = 'https://cicd-assignment1-kappa.vercel.app';
 
 const api = axios.create({
   baseURL: BASE_URL,
