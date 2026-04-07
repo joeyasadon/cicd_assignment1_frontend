dfbebb15bd39b8917e9f8ae4ddfa58d78b882ba0
Joey <joas001@ess.ais.ac.nz>
2026-04-08 11:52:53 +1200

    Fix the correct the backend url

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://cicd-assignment1-kappa.vercel.app';
+const BASE_URL = process.env.REACT_APP_API_URL || 'https://cicd-assignment1-kappa.vercel.app';
 
 const api = axios.create({
   baseURL: BASE_URL,
