463e40f1c300c373da18b035b00b25de0e6eb5bf
Joey <joas001@ess.ais.ac.nz>
2026-04-08 12:39:05 +1200

    Fix .env the correct the backend url. Test change part2

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'https://cicd-assignment1-kappa.vercel.app';
+const BASE_URL = 'https://cicd-assignment1-kappa.vercel.app';
 console.log('BASE_URL being used:', BASE_URL);
 console.log('REACT_APP_API_URL env var:', process.env.REACT_APP_API_URL);
 
